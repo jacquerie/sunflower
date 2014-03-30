0802fb290f4ee58a9025e07e76f276b13ec5b67d
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2014-03-30 05:30:30 +0200

    Stub now working.

--- js/application.js
+++ js/application.js
@@ -1,12 +1,11 @@
 // Set up SVG for D3.
-var width = 570,
+var margins = { left: 35, right: 35 },
+    width = 570,
     height = 550;
 
 var svg = d3.select("svg")
     .attr("width", width)
     .attr("height", height)
-
-var SEED_RADIUS = 5;
 
 // Let F_n be the nth Fibonacci number. We can prove by
 // induction that \sum_{i=1}^{n}{F_i} = F_{n+2} - 1. Then
@@ -28,16 +27,77 @@ function placeSeed (n, alpha) {
   return { r: r * Math.cos(phi), phi: r * Math.sin(phi) };
 }
 
-// d3.range(start, end) returns the elements from start to end - 1.
-// We add back one to have NUM_SEEDS seeds.
-var seeds = d3.range(1, NUM_SEEDS + 1).map(function (n) {
-  return placeSeed(n, GOLDEN_ANGLE);
-});
+function restart (alpha) {
+  var SEED_RADIUS = 5;
 
-svg.selectAll("circle")
-    .data(seeds)
-  .enter().append("circle")
-    .attr("cx", function(d) { return width / 2 + d.r; })
-    .attr("cy", function(d) { return height / 2 + d.phi; })
-    .attr("r", SEED_RADIUS);
+  // d3.range(start, end) returns the elements from start to end - 1.
+  // We add back one to have NUM_SEEDS seeds.
+  var seeds = d3.range(0, NUM_SEEDS + 1).map(function (n) {
+    return placeSeed(n, alpha);
+  });
 
+  svg.selectAll(".seed")
+      .remove();
+
+  svg.selectAll("circle")
+      .data(seeds)
+    .enter().append("circle")
+      .attr("class", "seed")
+      .attr("cx", function(d) { return width / 2 + d.r; })
+      .attr("cy", function(d) { return height / 2 + d.phi; })
+      .attr("r", SEED_RADIUS)
+}
+
+var x = d3.scale.linear()
+    .domain([0, 180])
+    .range([margins.left, width - margins.right])
+    .clamp(true);
+
+var brush = d3.svg.brush()
+    .x(x)
+    .extent([0,0])
+    .on("brush", brushed);
+
+svg.append("g")
+    .attr("class", "axis")
+    .attr("transform", "translate(0," + (5 * height) / 6 + ")")
+    .call(d3.svg.axis()
+      .scale(x)
+      .orient("bottom")
+      .tickFormat(function (d) { return d + "°"; })
+      .tickSize(0)
+      .tickPadding(12))
+    .select(".domain")
+    .select(function () { return this.parentNode.appendChild(this.cloneNode(true)); })
+      .attr("class", "halo");
+
+var slider = svg.append("g")
+    .attr("class", "slider")
+    .call(brush);
+
+slider.selectAll(".extent,.resize")
+    .remove();
+
+slider.select(".background")
+    .attr("height", height);
+
+var handle = slider.append("circle")
+    .attr("class", "handle")
+    .attr("transform", "translate(0," + (5 * height) / 6 + ")")
+    .attr("r", 10);
+
+slider.call(brush.event)
+   .call(brush.extent([137.5, 137.5]))
+   .call(brush.event);
+
+function brushed () {
+  var value = brush.extent()[0];
+
+  if (d3.event.sourceEvent) {
+    value = x.invert(d3.mouse(this)[0]);
+    brush.extent([value, value]);
+  }
+
+  handle.attr("cx", x(value));
+  restart(2 * (value / 360) * Math.PI);
+}
